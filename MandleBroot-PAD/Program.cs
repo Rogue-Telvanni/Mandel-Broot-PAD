a70a60d0a45afb10a49cc98671ce9d99f85ecf20
Rogue-Telvanni <joaocopini11@gmail.com>
2026-06-07 21:24:34 -0300

    ajuste do git para o VS2026 e adicao do ray lib

--- MandleBroot-PAD/Program.cs
+++ MandleBroot-PAD/Program.cs
@@ -1,7 +1,22 @@
 ﻿using BenchmarkDotNet.Running;
-using MandleBroot_PAD;
+using Raylib_cs;
+
+
+Raylib.InitWindow(800, 480, "Hello World");
+
+while (!Raylib.WindowShouldClose())
+{
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.White);
+
+    Raylib.DrawText("Hello, world!", 12, 12, 20, Color.Black);
+
+    Raylib.EndDrawing();
+}
+
+Raylib.CloseWindow();
 
 //var size = (1200, 1200);
 // Mandelbrot.RunThreadsSimplesOtimizado("treadOtimizada.pgm", size);
 
-var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
+//_ = BenchmarkRunner.Run(typeof(Program).Assembly);
